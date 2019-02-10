e864dc667f0782f9b5d4cace55d69719f1d643f6
Maximo Bautista <max.bautista97@gmail.com>
2019-02-10 03:51:34 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -26,8 +26,6 @@ class Show < ActiveRecord::Base
   
 end
 
-# ratings_sum: returns the sum of all of the ratings.
-
 # popular_shows: returns an array of all of the shows that have a rating greater than 5. hint: use the where Active Record method.
 
 # shows_by_alphabetical_order: returns an array of all of the shows sorted by alphabetical order according to their names. hint: use the order Active Record method.
