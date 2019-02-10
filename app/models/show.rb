6b09686ce6d1084513aaebd8cb68477ca9793711
Maximo Bautista <max.bautista97@gmail.com>
2019-02-10 03:50:15 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -12,7 +12,7 @@ class Show < ActiveRecord::Base
     self.minimum(:rating)
   end
   
-  def self.most_popular_show
+  def self.least_popular_show
     self.find_by(rating: self.lowest_rating)
   end
   
